b664c10af2acf5d5ef0e94f8dacac94853f6560c
Lizy <LizyZZZ@163.com>
2023-11-12 18:47:57 +0800

    add test

--- test.cpp
+++ test.cpp
@@ -1,33 +1,25 @@
 #include "logging.h"
-
+#include <chrono>
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
   InitLogging(argv[0]);
   SetLogDir("/home/lizy/lizy_log/");
-  SetLogDestination(LOG_INFO, "test");
+  SetLogDestination(LOG_INFO, "testI");
+  SetLogDestination(LOG_WARNING, "testW");
+  SetLogDestination(LOG_ERROR, "testE");
   SetLogFilenameExtension(".log");
 
-  LogSink s;
-  LOG_TO_SINK(&s, WARNING) << "sink";
-
-  std::string str;
-  LOG_TO_STRING(INFO, &str) << "log to string";
-  std::vector<std::string> vec;
-  LOG_STRING(INFO, &vec) << "log to vec";
-
-  LOG_IF(INFO, false) << "log if";
-
-  int num = 100;
-  LOG(WARNING) << "lizy" << num;
-  LOG(ERROR) << "lizy" << num + 1;
-  for (int i = 0; i < 100; i++) {
-    LOG(INFO) << "hello log";
+  int epi = 50000;
+  auto start = std::chrono::system_clock::now();
+  for (int i = 0; i < epi; i++) {
+    LOG(INFO) << "hello log" << i;
   }
+  auto end = std::chrono::system_clock::now();
 
-  int a = 1;
-  int b = 2;
-  // CHECK(a == b);
+  auto res = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+  std::cout << "QPS: " <<  epi / res.count() << "msg/s" << std::endl;
 
   return 0;
 }
